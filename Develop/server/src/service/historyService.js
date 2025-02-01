f3b07bc1e7bb4124899eb9410cf22514f0b42286
Isaac Najera <isaacnajera@isaacs-air.mynetworksettings.com>
2025-01-31 21:43:53 -0700

    initial commit 2

---
+++ Develop/server/src/service/historyService.js
@@ -0,0 +1,12 @@
+// TODO: Define a City class with name and id properties
+// TODO: Complete the HistoryService class
+class City {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    };
+};
+
+class HistoryService {
+}
+export default new HistoryService();
